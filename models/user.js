17384c4f60ea91e1e61c0b7a50d8334ce1d08c18
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-05-17 19:47:10 -0400

    Add Hash

--- models/user.js
+++ models/user.js
@@ -1,12 +1,13 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // Create our User Model 
-class User extends Model {}
-    User.init(
-        {
-          // define an id column
-          id: {
+class User extends Model { }
+User.init(
+    {
+        // define an id column
+        id: {
             // use the special Sequelize DataTypes object provide what type of data it is
             type: DataTypes.INTEGER,
             // this is the equivalent of SQL's `NOT NULL` option
@@ -15,42 +16,56 @@ class User extends Model {}
             primaryKey: true,
             // turn on auto increment
             autoIncrement: true
-          },
-          // define a username column
-          username: {
+        },
+        // define a username column
+        username: {
             type: DataTypes.STRING,
             allowNull: false
-          },
-          // define an email column
-          email: {
+        },
+        // define an email column
+        email: {
             type: DataTypes.STRING,
             allowNull: false,
             // there cannot be any duplicate email values in this table
             unique: true,
             // if allowNull is set to false, we can run our data through validators before creating the table data
             validate: {
-              isEmail: true
+                isEmail: true
             }
-          },
-          // define a password column
-          password: {
+        },
+        // define a password column
+        password: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-              // this means the password must be at least four characters long
-              len: [4]
+                // this means the password must be at least four characters long
+                len: [4]
             }
-          }
-        },
-        {
-          sequelize,
-          timestamps: false,
-          freezeTableName: true,
-          underscored: true,
-          modelName: 'user'
         }
-      );
-      
+    },
+    {
+        hooks: {
+            //set up before Create lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
 
 
 // Define Table Columns & Configuration
@@ -61,20 +76,19 @@ User.init(
     {
         // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
-    // pass in our imported sequelize connection (the direct connection to our database)
-    sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
-    // don't pluralize name of database table
-    freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
-    // make it so our model name stays lowercase in the database
-    modelName: 'user'
-  }
+        // pass in our imported sequelize connection (the direct connection to our database)
+        sequelize,
+        // don't automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        // don't pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+        underscored: true,
+        // make it so our model name stays lowercase in the database
+        modelName: 'user'
+    }
 );
 
 
 
 module.exports = User;
-    
